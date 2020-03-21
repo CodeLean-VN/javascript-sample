6817c9e2cfa979862940e8290605c3d7ead0c832
CodeLean-VN <codeleanvn@gmail.com>
2020-03-21 08:27:49 +0700

    Cac vi du ve JavaScript

---
+++ bai2/myFunctions.js
@@ -0,0 +1,17 @@
+//1. Muc dich: Tinh tong cua 2 so bat ky
+//2. Ten ham: tinhTong
+//3: Tham so dau vao: 2 so bat ky
+//4: Kieu du lieu tra ve cua ham: tra ve mot so
+
+/*
+--Cu phap tao ham trong JavaScript
+function tenHam(tham_so_dau_vao1, tham_so_dau_vao2, .....){
+    //cac lenh - than ham
+    return gia_tri;
+}*/
+
+//Dinh nghia ham HAY Tao ham
+function tinhTong(firstNumber, secondNumber) {
+    var total = firstNumber + secondNumber;
+    return total;
+}
